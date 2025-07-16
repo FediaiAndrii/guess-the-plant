cfec84b42feee846aacf01d9825009b7657a2391
Fediai Andrii <127394326+FediaiAndrii@users.noreply.github.com>
2025-07-16 16:21:32 +0300

    fix images bug

---
+++ scripts/plants.js
@@ -0,0 +1,63 @@
+"use strict";
+let plants = [
+  {
+    name: "African Violet",
+    secondName: "Saintpaulia",
+    image: "assets/images/african-violet-saintpaulia.png",
+  },
+  {
+    name: "Aloe Vera",
+    secondName: "Aloe Vera",
+    image: "assets/images/aloe-vera-aloe-vera.png",
+  },
+  {
+    name: "Anthurium",
+    secondName: "Anthurium Andraeanum",
+    image: "assets/images/anthurium-anthurium-andraeanum.png",
+  },
+  {
+    name: "Calathea",
+    secondName: "Calathea",
+    image: "assets/images/calathea-calathea.png",
+  },
+  {
+    name: "Chines Money Plant",
+    secondName: "Pilea Peperomioides",
+    image: "assets/images/chinese-money-plant-pilea-peperomioides.png",
+  },
+  {
+    name: "Dracaena",
+    secondName: "Dracaena Fragrans",
+    image: "assets/images/dracaena-dracaena-fragrans.png",
+  },
+  {
+    name: "Echeveria",
+    secondName: "Echeveria Succulent",
+    image: "assets/images/echeveria-echeveria-succulent.png",
+  },
+  {
+    name: "Love Tree",
+    secondName: "Aichryson Tree Of Love",
+    image: "assets/images/love-tree-aichryson-tree-of-love.png",
+  },
+  {
+    name: "Mammillaria",
+    secondName: "Cactus Mammillaria",
+    image: "assets/images/mammillaria-cactus-mammillaria.png",
+  },
+  {
+    name: "Monstera",
+    secondName: "Monstera Deliciosa",
+    image: "assets/images/monstera-monstera-deliciosa.png",
+  },
+  {
+    name: "Rubber Plant",
+    secondName: "Ficus Elastica",
+    image: "assets/images/rubber-plant-ficus-elastica.png",
+  },
+  {
+    name: "ZZ Plant",
+    secondName: "Zamioculcas Zamifolia",
+    image: "assets/images/zz-plant-zamioculcas-zamiifolia.png",
+  },
+];
